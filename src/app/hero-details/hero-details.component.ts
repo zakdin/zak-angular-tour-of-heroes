90fac94f0d33218492d628b9f12144fb06c5e84d
zakdin <mailbanu2211@gmail.com>
2021-03-16 23:11:25 +0530

    1 file modified

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -6,7 +6,6 @@ import { Hero } from "../hero";
   templateUrl: "./hero-details/hero-details.component.html",
   styleUrls: ["./hero-details/hero-details.component.css"]
 })
-
 export class HeroDetailsComponent implements OnInit {
   ngOnInit(): void {
     throw new Error("Method not implemented.");
