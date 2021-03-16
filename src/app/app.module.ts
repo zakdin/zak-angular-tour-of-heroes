0a9d2de2d2264fefea55f987e2f90fc8d33a2021
zakdin <mailbanu2211@gmail.com>
2021-03-16 23:15:55 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
-
-import { HeroDetailsComponent } from "./hero-details/hero-details.component";
+import { HeroDetailComponent } from "./hero-details/hero-details.component";
 
 @NgModule({
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroesComponent, HeroDetailsComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
