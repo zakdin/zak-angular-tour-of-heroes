d8a1c2ad2e749bdb2c30569e0f5042c4442ef591
zakdin <mailbanu2211@gmail.com>
2021-03-16 23:16:39 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
-import { HeroDetailComponent } from "./hero-details/hero-details.component";
+import { HeroDetailsComponent } from "./hero-details/hero-details.component";
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent, HeroDetailsComponent],
